6951a1f9115584fcc050a91ee82f83ac812436f9
Tiago Almeida <tyagow@hotmail.com.br>
2018-02-20 15:39:06 -0300

    feat: adicionando test a view settings

--- src/accounts/tests_accounts/test_view.py
+++ src/accounts/tests_accounts/test_view.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import resolve_url as r
 from django.test import TestCase
 
@@ -27,4 +28,27 @@ class LoginTest(TestCase):
     def test_template(self):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, 'registration/login.html')
+
+
+class SettingsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            "user123",
+            "user@example.com",
+            "password123")
+
+    def test_get(self):
+        self.assertTrue(self.client.login(username='user123', password='password123'))
+
+        self.response = self.client.get(r('accounts:settings'))
+
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use registration/settings.html"""
+
+        self.assertTrue(self.client.login(username='user123', password='password123'))
+
+        self.response = self.client.get(r('accounts:settings'))
+        self.assertTemplateUsed(self.response, 'accounts/settings.html')
 
